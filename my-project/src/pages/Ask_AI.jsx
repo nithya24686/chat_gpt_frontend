373368aac234f70a9842cbc7b578adaa602deeac
nithya <tmnithya9812gmail.com>
2026-02-13 11:35:09 +0530

    commit

--- my-project/src/pages/Ask_AI.jsx
+++ my-project/src/pages/Ask_AI.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 const Ask_AI = () => {
     const navigate = useNavigate();
--- my-project/src/pages/Signup.jsx
+++ my-project/src/pages/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
     setSuccess(false);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/signup', {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE}/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
